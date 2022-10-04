41b86f12510041ecc9d98567e6873e0f906f04ec
Frank Kunkel <fkunkel@proofpoint.com>
2022-10-04 19:17:23 -0400

    refactor handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,32 +1,20 @@
 package handlers
 
 import (
-	"database/sql"
 	"github.com/fkunkel/go_api/domain"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
 )
+
 type Env struct {
-	logger *zerolog.Logger
-	companys interface{
+	logger   *zerolog.Logger
+	companys interface {
 		All() ([]domain.Company, error)
 	}
 }
 
-func Routers() *mux.Router {
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	logger := zerolog.Logger{}
+func (env *Env) Routers() *mux.Router {
 
-	db, err := sql.Open("mysql", "platformUser:Wombat2016#@/platformtest")
-	if err != nil {
-		logger.Error().Msg("Cannot make connection")
-	}
-
-	// See "Important settings" section.
-	// db.SetConnMaxLifetime(3 * time.Minute)
-	// db.SetMaxOpenConns(10)
-	// db.SetMaxIdleConns(10)
-	env := &Env{logger: &logger,companys: domain.CompanyModel{db}}
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home).Methods("GET")
 	r.HandleFunc("/health", health).Methods("GET")
